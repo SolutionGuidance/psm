ec0e255a0fd4ff1b264b5c2be382d8047cf2248a
James Vasile <james@jamesvasile.com>
2017-05-23 08:54:10 -0400

    Dockerize wildfly and psm

---
+++ docker/wildfly/bin/cmd.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Make sure we're in Docker, not at command line
+if ! grep --quiet docker /proc/1/cgroup; then
+		echo This script should be run from within the wildfly Docker container.
+		echo No need for you to run it manually!
+		exit
+fi
+
+function wait_for_mail() {
+		echo "Waiting for mail server to come up."
+		until `echo "QUIT" | nc -w 5 mail 25 | grep -q "221 Ok"`; do
+				sleep 1
+		done
+		echo "Mail server is up."
+}
+
+function wait_for_db {
+		echo "Waiting for db server to come up."
+		until false; do
+				/usr/pgsql-9.6/bin/pg_isready -h db && return
+				sleep 1
+		done
+}
+
+[ -z "$(ls -A /opt/jboss/wildfly)" ] && cp -r /opt/jboss/wildfly.static/* /opt/jboss/wildfly
+/root/bin/build.sh
+/root/bin/setup.sh
+wait_for_db
+wait_for_mail
+
+echo "Starting wildfly server!"
+${JBOSS_HOME}/bin/standalone.sh -c standalone-full.xml -bmanagement 0.0.0.0 -b 0.0.0.0
---
+++ docker/wildfly/bin/setup.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# Make sure we're in Docker, not at command line
+if ! grep --quiet docker /proc/1/cgroup; then
+		echo This script should be run from within the wildfly Docker container.
+		echo No need for you to run it manually!
+		exit
+fi
+
+function wait_for_jboss() {
+  until `$JBOSS_CLI -c "ls /deployment" &> /dev/null`; do
+			sleep 1
+			echo Waiting for wildfly to come up.
+  done
+}
+
+# If our config is newer than the somewhat randomly-chosen
+# bin/product.conf, it indicates we've already done setup.
+[[ ${JBOSS_HOME}/standalone/configuration/standalone-full.xml -nt ${JBOSS_HOME}/bin/product.conf  ]] && exit
+
+${JBOSS_HOME}/bin/standalone.sh -c standalone-full.xml -bmanagement 0.0.0.0 -b 0.0.0.0 &
+wait_for_jboss
+
+# Setup mail server
+${JBOSS_CLI} --connect << EOF
+/socket-binding-group=standard-sockets/remote-destination-outbound-socket-binding=mail-smtp:write-attribute(name=port,value=25)
+/socket-binding-group=standard-sockets/remote-destination-outbound-socket-binding=mail-smtp:write-attribute(name=host,value="mail")
+/subsystem=mail/mail-session="java:/Mail":add(jndi-name="java:/Mail")
+/subsystem=mail/mail-session="java:/Mail"/server=smtp:add(outbound-socket-binding-ref=mail-smtp)
+EOF
+
+# Create message queue
+${JBOSS_CLI} --connect --command='jms-queue add --queue-address=DataSync --entries=["java:/jms/queue/DataSync"]'
+
+# Set node identifier to unique value
+${JBOSS_CLI} --connect << EOF
+/subsystem=transactions:write-attribute(name=node-identifier,value=QuietWarning)
+EOF
+
+# Connect the Postgres JDBC driver to Wildfly
+${JBOSS_CLI} --connect --command="deploy /usr/share/java/postgresql.jar"
+
+# Load data
+${JBOSS_CLI} --connect <<EOF
+xa-data-source add \
+  --name=TaskServiceDS \
+  --jndi-name=java:/jdbc/TaskServiceDS \
+  --driver-name=postgresql.jar \
+  --xa-datasource-class=org.postgresql.xa.PGXADataSource \
+  --valid-connection-checker-class-name=org.jboss.jca.adapters.jdbc.extensions.postgres.PostgreSQLValidConnectionChecker \
+  --exception-sorter-class-name=org.jboss.jca.adapters.jdbc.extensions.postgres.PostgreSQLExceptionSorter \
+  --enabled=true \
+  --use-ccm=true \
+  --background-validation=true \
+  --user-name=psm \
+  --password=psm \
+  --xa-datasource-properties=ServerName=db,PortNumber=5432,DatabaseName=psm
+xa-data-source add \
+  --name=MitaDS \
+  --jndi-name=java:/jdbc/MitaDS \
+  --driver-name=postgresql.jar \
+  --xa-datasource-class=org.postgresql.xa.PGXADataSource \
+  --valid-connection-checker-class-name=org.jboss.jca.adapters.jdbc.extensions.postgres.PostgreSQLValidConnectionChecker \
+  --exception-sorter-class-name=org.jboss.jca.adapters.jdbc.extensions.postgres.PostgreSQLExceptionSorter \
+  --enabled=true \
+  --use-ccm=true \
+  --background-validation=true \
+  --user-name=psm \
+  --password=psm \
+  --xa-datasource-properties=ServerName=db,PortNumber=5432,DatabaseName=psm
+EOF
+
+$JBOSS_CLI -c ":shutdown"

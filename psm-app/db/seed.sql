6b9b110eb91d215041cffc2cd83465727077e49e
Jason Owen <jason@jasonaowen.net>
2017-05-29 13:40:22 -0400

    Switch states over to annotations
    Pluralize the table name, remove the `lu_` prefix, remove the empty
    constructor, remove the redundant comment, and add seed data.
    
    Question: the seed data contains the 50 states and the District of
    Columbia. Do we also need to include US territories, associated states,
    and other similar? See issue #62.
    
    https://en.wikipedia.org/wiki/List_of_U.S._state_abbreviations
    
    Issue #36 Use Hibernate 5, instead of 4

--- psm-app/db/seed.sql
+++ psm-app/db/seed.sql
@@ -8,7 +8,8 @@ DROP TABLE IF EXISTS
   cms_user,
   persistent_logins,
   provider_types,
-  roles
+  roles,
+  states
 CASCADE;
 
 CREATE SEQUENCE hibernate_sequence;
@@ -161,3 +162,60 @@ INSERT INTO provider_types(code, description, applicant_type) VALUES
   ('76', 'Birthing Center', 'ORGANIZATION'),
   ('77', 'Medical Transportation', 'ORGANIZATION'),
   ('78', 'Billing Entity for Physician Services', 'ORGANIZATION');
+
+CREATE TABLE states (
+   code CHARACTER VARYING(2) PRIMARY KEY,
+   description TEXT UNIQUE
+);
+INSERT INTO states (code, description) VALUES
+  ('AK', 'Alaska'),
+  ('AL', 'Alabama'),
+  ('AR', 'Arkansas'),
+  ('AZ', 'Arizona'),
+  ('CA', 'California'),
+  ('CO', 'Colorado'),
+  ('CT', 'Connecticut'),
+  ('DC', 'District of Columbia'),
+  ('DE', 'Delaware'),
+  ('FL', 'Florida'),
+  ('GA', 'Georgia'),
+  ('HI', 'Hawaii'),
+  ('IA', 'Iowa'),
+  ('ID', 'Idaho'),
+  ('IL', 'Illinois'),
+  ('IN', 'Indiana'),
+  ('KS', 'Kansas'),
+  ('KY', 'Kentucky'),
+  ('LA', 'Louisiana'),
+  ('MA', 'Massachusetts'),
+  ('MD', 'Maryland'),
+  ('ME', 'Maine'),
+  ('MI', 'Michigan'),
+  ('MN', 'Minnesota'),
+  ('MO', 'Missouri'),
+  ('MS', 'Mississippi'),
+  ('MT', 'Montana'),
+  ('NC', 'North Carolina'),
+  ('ND', 'North Dakota'),
+  ('NE', 'Nebraska'),
+  ('NH', 'New Hampshire'),
+  ('NJ', 'New Jersey'),
+  ('NM', 'New Mexico'),
+  ('NV', 'Nevada'),
+  ('NY', 'New York'),
+  ('OH', 'Ohio'),
+  ('OK', 'Oklahoma'),
+  ('OR', 'Oregon'),
+  ('PA', 'Pennsylvania'),
+  ('RI', 'Rhode Island'),
+  ('SC', 'South Carolina'),
+  ('SD', 'South Dakota'),
+  ('TN', 'Tennessee'),
+  ('TX', 'Texas'),
+  ('UT', 'Utah'),
+  ('VA', 'Virginia'),
+  ('VT', 'Vermont'),
+  ('WA', 'Washington'),
+  ('WI', 'Wisconsin'),
+  ('WV', 'West Virginia'),
+  ('WY', 'Wyoming');

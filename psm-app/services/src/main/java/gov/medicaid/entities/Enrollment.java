b90adf3d0a32f27b0cab7be9593b854a96f31840
Jason Owen <jason@jasonaowen.net>
2017-05-29 18:29:00 -0400

    Remove redundant comments from Enrollment

--- psm-app/services/src/main/java/gov/medicaid/entities/Enrollment.java
+++ psm-app/services/src/main/java/gov/medicaid/entities/Enrollment.java
@@ -1,7 +1,7 @@
 /*
  * Copyright 2012-2013 TopCoder, Inc.
  *
- * This code was developed under U.S. government contract NNH10CD71C. 
+ * This code was developed under U.S. government contract NNH10CD71C.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * You may not use this file except in compliance with the License.
@@ -61,9 +61,6 @@ public class Enrollment implements Serializable {
      */
     private Date referenceTimestamp;
 
-    /**
-     * The submission date.
-     */
     private Date submissionDate;
 
     /**
@@ -86,9 +83,6 @@ public class Enrollment implements Serializable {
      */
     private String createdBy;
 
-    /**
-     * Username that created this request.
-     */
     private Date createdOn;
 
     /**
@@ -107,264 +101,122 @@ public class Enrollment implements Serializable {
     public Enrollment() {
     }
 
-    /**
-     * Gets the value of the field <code>ticketId</code>.
-     *
-     * @return the ticketId
-     */
     public long getTicketId() {
         return ticketId;
     }
 
-    /**
-     * Sets the value of the field <code>ticketId</code>.
-     *
-     * @param ticketId the ticketId to set
-     */
     public void setTicketId(long ticketId) {
         this.ticketId = ticketId;
     }
 
-    /**
-     * Gets the value of the field <code>status</code>.
-     *
-     * @return the status
-     */
     public EnrollmentStatus getStatus() {
         return status;
     }
 
-    /**
-     * Sets the value of the field <code>status</code>.
-     *
-     * @param status the status to set
-     */
     public void setStatus(EnrollmentStatus status) {
         this.status = status;
     }
 
-    /**
-     * Gets the value of the field <code>requestType</code>.
-     *
-     * @return the requestType
-     */
     public RequestType getRequestType() {
         return requestType;
     }
 
-    /**
-     * Sets the value of the field <code>requestType</code>.
-     *
-     * @param requestType the requestType to set
-     */
     public void setRequestType(RequestType requestType) {
         this.requestType = requestType;
     }
 
-    /**
-     * Gets the value of the field <code>statusDate</code>.
-     *
-     * @return the statusDate
-     */
     public Date getStatusDate() {
         return statusDate;
     }
 
-    /**
-     * Sets the value of the field <code>statusDate</code>.
-     *
-     * @param statusDate the statusDate to set
-     */
     public void setStatusDate(Date statusDate) {
         this.statusDate = statusDate;
     }
 
-    /**
-     * Gets the value of the field <code>statusNote</code>.
-     *
-     * @return the statusNote
-     */
     public String getStatusNote() {
         return statusNote;
     }
 
-    /**
-     * Sets the value of the field <code>statusNote</code>.
-     *
-     * @param statusNote the statusNote to set
-     */
     public void setStatusNote(String statusNote) {
         this.statusNote = statusNote;
     }
 
-    /**
-     * Gets the value of the field <code>submittedBy</code>.
-     *
-     * @return the submittedBy
-     */
     public String getSubmittedBy() {
         return submittedBy;
     }
 
-    /**
-     * Sets the value of the field <code>submittedBy</code>.
-     *
-     * @param submittedBy the submittedBy to set
-     */
     public void setSubmittedBy(String submittedBy) {
         this.submittedBy = submittedBy;
     }
 
-    /**
-     * Gets the value of the field <code>lastUpdatedBy</code>.
-     *
-     * @return the lastUpdatedBy
-     */
     public String getLastUpdatedBy() {
         return lastUpdatedBy;
     }
 
-    /**
-     * Sets the value of the field <code>lastUpdatedBy</code>.
-     *
-     * @param lastUpdatedBy the lastUpdatedBy to set
-     */
     public void setLastUpdatedBy(String lastUpdatedBy) {
         this.lastUpdatedBy = lastUpdatedBy;
     }
 
-    /**
-     * Gets the value of the field <code>details</code>.
-     *
-     * @return the details
-     */
     public ProviderProfile getDetails() {
         return details;
     }
 
-    /**
-     * Sets the value of the field <code>details</code>.
-     *
-     * @param details the details to set
-     */
     public void setDetails(ProviderProfile details) {
         this.details = details;
     }
 
-    /**
-     * Gets the value of the field <code>processInstanceId</code>.
-     *
-     * @return the processInstanceId
-     */
     public long getProcessInstanceId() {
         return processInstanceId;
     }
 
-    /**
-     * Sets the value of the field <code>processInstanceId</code>.
-     *
-     * @param processInstanceId the processInstanceId to set
-     */
     public void setProcessInstanceId(long processInstanceId) {
         this.processInstanceId = processInstanceId;
     }
 
-    /**
-     * Gets the value of the field <code>submissionDate</code>.
-     *
-     * @return the submissionDate
-     */
     public Date getSubmissionDate() {
         return submissionDate;
     }
 
-    /**
-     * Sets the value of the field <code>submissionDate</code>.
-     *
-     * @param submissionDate the submissionDate to set
-     */
     public void setSubmissionDate(Date submissionDate) {
         this.submissionDate = submissionDate;
     }
 
-    /**
-     * Gets the value of the field <code>createdBy</code>.
-     *
-     * @return the createdBy
-     */
     public String getCreatedBy() {
         return createdBy;
     }
 
-    /**
-     * Sets the value of the field <code>createdBy</code>.
-     *
-     * @param createdBy the createdBy to set
-     */
     public void setCreatedBy(String createdBy) {
         this.createdBy = createdBy;
     }
 
-    /**
-     * Gets the value of the field <code>profileReferenceId</code>.
-     * @return the profileReferenceId
-     */
     public long getProfileReferenceId() {
         return profileReferenceId;
     }
 
-    /**
-     * Sets the value of the field <code>profileReferenceId</code>.
-     * @param profileReferenceId the profileReferenceId to set
-     */
     public void setProfileReferenceId(long profileReferenceId) {
         this.profileReferenceId = profileReferenceId;
     }
 
-    /**
-     * Gets the value of the field <code>createdOn</code>.
-     * @return the createdOn
-     */
     public Date getCreatedOn() {
         return createdOn;
     }
 
-    /**
-     * Sets the value of the field <code>createdOn</code>.
-     * @param createdOn the createdOn to set
-     */
     public void setCreatedOn(Date createdOn) {
         this.createdOn = createdOn;
     }
 
-    /**
-     * Gets the value of the field <code>progressPage</code>.
-     * @return the progressPage
-     */
     public String getProgressPage() {
         return progressPage;
     }
 
-    /**
-     * Sets the value of the field <code>progressPage</code>.
-     * @param progressPage the progressPage to set
-     */
     public void setProgressPage(String progressPage) {
         this.progressPage = progressPage;
     }
 
-    /**
-     * Gets the value of the field <code>referenceTimestamp</code>.
-     * @return the referenceTimestamp
-     */
     public Date getReferenceTimestamp() {
         return referenceTimestamp;
     }
 
-    /**
-     * Sets the value of the field <code>referenceTimestamp</code>.
-     * @param referenceTimestamp the referenceTimestamp to set
-     */
     public void setReferenceTimestamp(Date referenceTimestamp) {
         this.referenceTimestamp = referenceTimestamp;
     }

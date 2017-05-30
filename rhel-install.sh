decaaf7b4f5fe4de15e243d51662063ce93aaa11
Dan Schultz <slifty@gmail.com>
2017-05-30 01:25:05 -0400

    Script improvements and PSQL 9.6
    This commit addresses most of the feedback provided in GH-42:
    
    - Upgrading the version of Postgres to 9.6 (from 9.2)
    - Fixing an issue where checksum would still exit on success
    - Fixing an issue where waiting for jboss would never resolve
    - Consolidating related yum installs
    - Adding some additional comments to document sections of the script
    - Adding some output to reflect script progress

--- rhel-install.sh
+++ rhel-install.sh
@@ -1,43 +1,55 @@
 #!/bin/bash
+set -e
 
 function download_and_sha1 {
 		base=$(basename $1)
 		[ -e $base ] || curl -OL $1
-		echo "$2 $base" | sha1sum --quiet -c - || printf "$base has an unexpected sha1sum: $(sha1sum $base | awk '{print $1}')\n" && exit
+		echo "$2 $base" | sha1sum --quiet -c - || (printf "$base has an unexpected sha1sum: $(sha1sum $base | awk '{print $1}')\n" && exit)
 }
 
-function wait_for_jboss() {
-	until `$JBOSS_CLI -c "ls /deployment" &> /dev/null`; do
-		sleep 1
-		echo Waiting for wildfly to come up.
+function wait_for_jboss {
+	until `$1 -c "ls /deployment" &> /dev/null`; do
+		sleep 5
+    echo "Waiting for wildfly to come up."
 	done
 }
 
 sudo yum -y update
+
+## Generate a password to use when passwords are needed
 < /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c30 | awk '{print $1"!"}' > pass.txt
-
 ## Install posgresql repo, then postgres
-download_and_sha1 "https://download.postgresql.org/pub/repos/yum/9.2/redhat/rhel-7-x86_64/pgdg-redhat92-9.2-3.noarch.rpm" \
-									f1cd62458abcc78f696f00aa5777cb4fc6f5fc05
-sudo rpm -ivh pgdg-redhat92-9.2-3.noarch.rpm
-rm pgdg-redhat92-9.2-3.noarch.rpm
-sudo yum -y install postgresql92-server postgresql92-contrib postgresql92-devel
+echo "Installing postgres"
+download_and_sha1 "https://download.postgresql.org/pub/repos/yum/9.6/redhat/rhel-7-x86_64/pgdg-redhat96-9.6-3.noarch.rpm" \
+									43715d4c1d7fcf8d5d825b24c5f62c08cbca040d
+echo "Downloaded"
+sudo rpm -ivh pgdg-redhat96-9.6-3.noarch.rpm
+rm pgdg-redhat96-9.6-3.noarch.rpm
+sudo yum -y install postgresql96-server postgresql96-contrib postgresql96-devel
 
-sudo /usr/pgsql-9.2/bin/postgresql92-setup initdb
-sudo sed -i 's/ident/md5/g' /var/lib/pgsql/9.2/data/pg_hba.conf
-sudo systemctl enable postgresql-9.2.service
-sudo systemctl start postgresql-9.2.service
+sudo /usr/pgsql-9.6/bin/postgresql96-setup initdb
+sudo sed -i 's/ident/md5/g' /var/lib/pgsql/9.6/data/pg_hba.conf
+sudo systemctl enable postgresql-9.6.service
+sudo systemctl start postgresql-9.6.service
 sudo -upostgres psql -c "create user \"psm\" with password '$(< pass.txt)'"
 sudo -upostgres psql -c "create database \"psm\" with owner \"psm\" encoding='utf8' template template0"
+
+## Get the project code base
+echo "Getting the project code base"
 sudo yum -y install git
 git clone https://github.com/OpenTechStrategies/psm.git
-sudo yum -y install java-1.8.0-openjdk
-sudo yum -y install ant
+
+## Install java dependencies
+echo "Installing java dependencies"
+sudo yum -y install java-1.8.0-openjdk ant
+
+## Install and run mailcatcher
+echo "Installing mailcatcher"
 sudo yum-config-manager --enable rhui-REGION-rhel-server-optional
-sudo yum -y install gcc mysql-devel ruby-devel rubygems
+sudo yum -y install gcc mysql-devel ruby-devel rubygems sqlite-devel
 sudo yum -y groupinstall 'Development Tools'
-sudo yum -y install sqlite-devel
 gem install mailcatcher
+echo "Running mailcatcher"
 mailcatcher
 
 ## Download and install wildfly
@@ -46,6 +58,7 @@ download_and_sha1 "http://download.jboss.org/wildfly/10.1.0.Final/wildfly-10.1.0
 tar -xzf wildfly-10.1.0.Final.tar.gz
 rm wildfly-10.1.0.Final.tar.gz
 
+## Set up wildfly server
 cat pass.txt | awk '{print "psm "$1}'>> script.txt
 eval ./wildfly-10.1.0.Final/bin/add-user.sh "$(< script.txt)"
 rm script.txt
@@ -53,7 +66,9 @@ rm script.txt
     -c standalone-full.xml \
     -b 0.0.0.0 \
     -bmanagement 0.0.0.0 &
-wait_for_jboss
+wait_for_jboss ./wildfly-10.1.0.Final/bin/jboss-cli.sh
+
+## Configure wildfly service bindings
 ./wildfly-10.1.0.Final/bin/jboss-cli.sh --connect << EOF
 /socket-binding-group=standard-sockets/remote-destination-outbound-socket-binding=mail-smtp:write-attribute(name=port,value=1025)
 /subsystem=mail/mail-session="java:/Mail":add(jndi-name="java:/Mail")
@@ -61,14 +76,14 @@ wait_for_jboss
 EOF
 ./wildfly-10.1.0.Final/bin/jboss-cli.sh --connect \
   --command='jms-queue add --queue-address=DataSync --entries=["java:/jms/queue/DataSync"]'
-download_and_sha1 "https://jdbc.postgresql.org/download/postgresql-9.2-1004.jdbc4.jar" \
-									c0b40281d2d6cc310e188730c488ac48138638cc
-./wildfly-10.1.0.Final/bin/jboss-cli.sh --connect --command="deploy postgresql-9.2-1004.jdbc4.jar"
+download_and_sha1 "https://jdbc.postgresql.org/download/postgresql-42.1.1.jar" \
+									8a0b76d763f5382d6357c412eeb14970ba4405f3
+./wildfly-10.1.0.Final/bin/jboss-cli.sh --connect --command="deploy postgresql-42.1.1.jar"
 ./wildfly-10.1.0.Final/bin/jboss-cli.sh --connect <<EOF
 xa-data-source add \
   --name=TaskServiceDS \
   --jndi-name=java:/jdbc/TaskServiceDS \
-  --driver-name=postgresql-9.2-1004.jdbc4.jar \
+  --driver-name=postgresql-42.1.1.jar \
   --xa-datasource-class=org.postgresql.xa.PGXADataSource \
   --valid-connection-checker-class-name=org.jboss.jca.adapters.jdbc.extensions.postgres.PostgreSQLValidConnectionChecker \
   --exception-sorter-class-name=org.jboss.jca.adapters.jdbc.extensions.postgres.PostgreSQLExceptionSorter \
@@ -81,7 +96,7 @@ xa-data-source add \
 xa-data-source add \
   --name=MitaDS \
   --jndi-name=java:/jdbc/MitaDS \
-  --driver-name=postgresql-9.2-1004.jdbc4.jar \
+  --driver-name=postgresql-42.1.1.jar \
   --xa-datasource-class=org.postgresql.xa.PGXADataSource \
   --valid-connection-checker-class-name=org.jboss.jca.adapters.jdbc.extensions.postgres.PostgreSQLValidConnectionChecker \
   --exception-sorter-class-name=org.jboss.jca.adapters.jdbc.extensions.postgres.PostgreSQLExceptionSorter \
@@ -92,6 +107,8 @@ xa-data-source add \
   --password=$(< pass.txt) \
   --xa-datasource-properties=ServerName=localhost,PortNumber=5432,DatabaseName=psm
 EOF
+
+## Build and deploy the psm app
 cp psm/psm-app/build.properties.template psm/psm-app/build.properties
 cd psm/psm-app
 ant dist

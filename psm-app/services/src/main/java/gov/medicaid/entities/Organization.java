bc99e84d23c3fe5ebd0ef6729ef3644eb9433160
Jason Owen <jason@jasonaowen.net>
2017-06-05 14:44:58 -0400

    Remove redundant comments from Organization

--- psm-app/services/src/main/java/gov/medicaid/entities/Organization.java
+++ psm-app/services/src/main/java/gov/medicaid/entities/Organization.java
@@ -15,14 +15,6 @@
  */
 package gov.medicaid.entities;
 
-
-
-/**
- * Represents an organization.
- *
- * @author TCSASSEMBLER
- * @version 1.0
- */
 public class Organization extends Entity {
 
     /**
@@ -75,14 +67,8 @@ public class Organization extends Entity {
      */
     private String fiscalYearEnd;
 
-    /**
-     * Remittance sequence order.
-     */
     private RemittanceSequenceOrder remittanceSequenceOrder;
 
-    /**
-     * EFT Vendor number.
-     */
     private String eftVendorNumber;
 
     /**
@@ -91,220 +77,99 @@ public class Organization extends Entity {
     public Organization() {
     }
 
-    /**
-     * Gets the value of the field <code>fein</code>.
-     *
-     * @return the fein
-     */
     public String getFein() {
         return fein;
     }
 
-    /**
-     * Sets the value of the field <code>fein</code>.
-     *
-     * @param fein the fein to set
-     */
     public void setFein(String fein) {
         this.fein = fein;
     }
 
-    /**
-     * Gets the value of the field <code>billingAddress</code>.
-     *
-     * @return the billingAddress
-     */
     public Address getBillingAddress() {
         return billingAddress;
     }
 
-    /**
-     * Sets the value of the field <code>billingAddress</code>.
-     *
-     * @param billingAddress the billingAddress to set
-     */
     public void setBillingAddress(Address billingAddress) {
         this.billingAddress = billingAddress;
     }
 
-    /**
-     * Gets the value of the field <code>reimbursementAddress</code>.
-     *
-     * @return the reimbursementAddress
-     */
     public Address getReimbursementAddress() {
         return reimbursementAddress;
     }
 
-    /**
-     * Sets the value of the field <code>reimbursementAddress</code>.
-     *
-     * @param reimbursementAddress the reimbursementAddress to set
-     */
     public void setReimbursementAddress(Address reimbursementAddress) {
         this.reimbursementAddress = reimbursementAddress;
     }
 
-    /**
-     * Gets the value of the field <code>ten99Address</code>.
-     *
-     * @return the ten99Address
-     */
     public Address getTen99Address() {
         return ten99Address;
     }
 
-    /**
-     * Sets the value of the field <code>ten99Address</code>.
-     *
-     * @param ten99Address the ten99Address to set
-     */
     public void setTen99Address(Address ten99Address) {
         this.ten99Address = ten99Address;
     }
 
-    /**
-     * Gets the value of the field <code>stateTaxId</code>.
-     *
-     * @return the stateTaxId
-     */
     public String getStateTaxId() {
         return stateTaxId;
     }
 
-    /**
-     * Sets the value of the field <code>stateTaxId</code>.
-     *
-     * @param stateTaxId the stateTaxId to set
-     */
     public void setStateTaxId(String stateTaxId) {
         this.stateTaxId = stateTaxId;
     }
 
-    /**
-     * Gets the value of the field <code>fiscalYearEnd</code>.
-     *
-     * @return the fiscalYearEnd
-     */
     public String getFiscalYearEnd() {
         return fiscalYearEnd;
     }
 
-    /**
-     * Sets the value of the field <code>fiscalYearEnd</code>.
-     *
-     * @param fiscalYearEnd the fiscalYearEnd to set
-     */
     public void setFiscalYearEnd(String fiscalYearEnd) {
         this.fiscalYearEnd = fiscalYearEnd;
     }
 
-    /**
-     * Gets the value of the field <code>remittanceSequenceOrder</code>.
-     *
-     * @return the remittanceSequenceOrder
-     */
     public RemittanceSequenceOrder getRemittanceSequenceOrder() {
         return remittanceSequenceOrder;
     }
 
-    /**
-     * Sets the value of the field <code>remittanceSequenceOrder</code>.
-     *
-     * @param remittanceSequenceOrder the remittanceSequenceOrder to set
-     */
     public void setRemittanceSequenceOrder(RemittanceSequenceOrder remittanceSequenceOrder) {
         this.remittanceSequenceOrder = remittanceSequenceOrder;
     }
 
-    /**
-     * Gets the value of the field <code>eftVendorNumber</code>.
-     *
-     * @return the eftVendorNumber
-     */
     public String getEftVendorNumber() {
         return eftVendorNumber;
     }
 
-    /**
-     * Sets the value of the field <code>eftVendorNumber</code>.
-     *
-     * @param eftVendorNumber the eftVendorNumber to set
-     */
     public void setEftVendorNumber(String eftVendorNumber) {
         this.eftVendorNumber = eftVendorNumber;
     }
 
-    /**
-     * Gets the value of the field <code>billingSameAsPrimary</code>.
-     *
-     * @return the billingSameAsPrimary
-     */
     public String getBillingSameAsPrimary() {
         return billingSameAsPrimary;
     }
 
-    /**
-     * Sets the value of the field <code>billingSameAsPrimary</code>.
-     *
-     * @param billingSameAsPrimary the billingSameAsPrimary to set
-     */
     public void setBillingSameAsPrimary(String billingSameAsPrimary) {
         this.billingSameAsPrimary = billingSameAsPrimary;
     }
 
-    /**
-     * Gets the value of the field <code>reimbursementSameAsPrimary</code>.
-     *
-     * @return the reimbursementSameAsPrimary
-     */
     public String getReimbursementSameAsPrimary() {
         return reimbursementSameAsPrimary;
     }
 
-    /**
-     * Sets the value of the field <code>reimbursementSameAsPrimary</code>.
-     *
-     * @param reimbursementSameAsPrimary the reimbursementSameAsPrimary to set
-     */
     public void setReimbursementSameAsPrimary(String reimbursementSameAsPrimary) {
         this.reimbursementSameAsPrimary = reimbursementSameAsPrimary;
     }
 
-    /**
-     * Gets the value of the field <code>ten99SameAsPrimary</code>.
-     *
-     * @return the ten99SameAsPrimary
-     */
     public String getTen99SameAsPrimary() {
         return ten99SameAsPrimary;
     }
 
-    /**
-     * Sets the value of the field <code>ten99SameAsPrimary</code>.
-     *
-     * @param ten99SameAsPrimary the ten99SameAsPrimary to set
-     */
     public void setTen99SameAsPrimary(String ten99SameAsPrimary) {
         this.ten99SameAsPrimary = ten99SameAsPrimary;
     }
 
-    /**
-     * Gets the value of the field <code>agencyId</code>.
-     *
-     * @return the agencyId
-     */
     public String getAgencyId() {
         return agencyId;
     }
 
-    /**
-     * Sets the value of the field <code>agencyId</code>.
-     *
-     * @param agencyId the agencyId to set
-     */
     public void setAgencyId(String agencyId) {
         this.agencyId = agencyId;
     }
-
 }

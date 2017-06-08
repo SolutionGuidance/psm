b5c328c25f2ca16e995948fb444157f91eaf85f3
Jason Owen <jason@jasonaowen.net>
2017-06-08 14:40:23 -0400

    Remove redundant comments from BinaryContent

--- psm-app/services/src/main/java/gov/medicaid/entities/BinaryContent.java
+++ psm-app/services/src/main/java/gov/medicaid/entities/BinaryContent.java
@@ -17,22 +17,9 @@ package gov.medicaid.entities;
 
 import java.sql.Blob;
 
-/**
- * Stores BLOB contents.
- *
- * @author TCSASSEMBLER
- * @version 1.0
- */
 public class BinaryContent {
-
-    /**
-     * The binary content.
-     */
     private Blob content;
 
-    /**
-     * The content id.
-     */
     private String contentId;
 
     /**
@@ -41,38 +28,18 @@ public class BinaryContent {
     public BinaryContent() {
     }
 
-    /**
-     * Gets the value of the field <code>content</code>.
-     *
-     * @return the content
-     */
     public Blob getContent() {
         return content;
     }
 
-    /**
-     * Sets the value of the field <code>content</code>.
-     *
-     * @param content the content to set
-     */
     public void setContent(Blob content) {
         this.content = content;
     }
 
-    /**
-     * Gets the value of the field <code>contentId</code>.
-     *
-     * @return the contentId
-     */
     public String getContentId() {
         return contentId;
     }
 
-    /**
-     * Sets the value of the field <code>contentId</code>.
-     *
-     * @param contentId the contentId to set
-     */
     public void setContentId(String contentId) {
         this.contentId = contentId;
     }

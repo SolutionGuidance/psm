SolutionGuidance/psm

commit 0fd71089ac8e2195e77471d31f2d138e8b2e337b

Switch RequiredFieldType over to annotations

psm-app/db/seed.sql
@@ -18,6 +18,7 @@ DROP TABLE IF EXISTS
   provider_profiles,
   provider_types,
   request_types,
+  required_field_type,
   risk_levels,
   roles,
   service_assurance_types,
@@ -201,6 +202,14 @@ INSERT INTO profile_statuses (code, description) VALUES
   ('01', 'Active'),
   ('02', 'Suspended'),
   ('03', 'Expired');
+
+CREATE TABLE required_field_type(
+  code CHARACTER VARYING(2) PRIMARY KEY,
+  description TEXT UNIQUE
+);
+INSERT INTO required_field_type (code, description) VALUES
+  ('01', 'Required'),
+  ('02', 'Optional'),
 
 CREATE TABLE risk_levels(
   code CHARACTER VARYING(2) PRIMARY KEY,

psm-app/cms-business-process/src/main/resources/META-INF/Medicaid.hbm.xml
@@ -878,16 +878,6 @@
 		<property column="CREATE_TS" generated="never" lazy="false"
 			name="createdOn" type="timestamp" />
 	</class>
-	<class name="gov.medicaid.entities.RequiredFieldType" table="REQUIRED_FLD_TYPE">
-		<id name="code" type="string">
-			<column length="2" name="CODE" />
-			<generator class="assigned" />
-		</id>
-		<property generated="never" lazy="false" length="200" name="description"
-			type="string">
-			<column name="DESCRIPTION" />
-		</property>
-	</class>
 	<class name="gov.medicaid.entities.RequiredField" table="REQUIRED_FLD">
 		<id name="code" type="string">
 			<column length="2" name="CODE" />

psm-app/cms-business-process/src/main/resources/META-INF/persistence.xml
@@ -56,6 +56,7 @@
     <class>gov.medicaid.entities.ProviderProfile</class>
     <class>gov.medicaid.entities.ProviderType</class>
     <class>gov.medicaid.entities.RequestType</class>
+    <class>gov.medicaid.entities.RequiredFieldType</class>
     <class>gov.medicaid.entities.RiskLevel</class>
     <class>gov.medicaid.entities.Role</class>
     <class>gov.medicaid.entities.ServiceAssuranceExtType</class>

psm-app/services/src/main/java/gov/medicaid/entities/RequiredFieldType.java
@@ -1,7 +1,7 @@
 /*
  * Copyright 2012-2013 TopCoder, Inc.
  *
- * This code was developed under U.S. government contract NNH10CD71C. 
+ * This code was developed under U.S. government contract NNH10CD71C.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * You may not use this file except in compliance with the License.
@@ -14,18 +14,8 @@
  * limitations under the License.
  */
 package gov.medicaid.entities;
+import javax.persistence.Table;
 
-/**
- * This represents a required field type.
- *
- * @author argolite, TCSASSEMBLER
- * @version 1.0
- */
-public class RequiredFieldType extends LookupEntity {
-
-    /**
-     * Default empty constructor.
-     */
-    public RequiredFieldType() {
-    }
-}
+@javax.persistence.Entity
+@Table(name = "required_field_type")
+public class RequiredFieldType extends LookupEntity {}

commit 170a18f0528e00eefbdc5d050288ba5676c6dc5a

Fix syntax error in seed.sql
Convert a trailing comma to a terminating semicolon. Typo introduced in
0fd71089ac8e2195e77471d31f2d138e8b2e337b.

psm-app/db/seed.sql
@@ -211,7 +211,7 @@ CREATE TABLE required_field_types(
 );
 INSERT INTO required_field_types (code, description) VALUES
   ('01', 'Required'),
-  ('02', 'Optional'),
+  ('02', 'Optional');
 
 CREATE TABLE risk_levels(
   code CHARACTER VARYING(2) PRIMARY KEY,
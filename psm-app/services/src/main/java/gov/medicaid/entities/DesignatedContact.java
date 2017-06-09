f16709f9261b8f08b33ed37a47e7b9fbaa065a98
Jason Owen <jason@jasonaowen.net>
2017-06-09 14:58:14 -0400

    Remove redundant comments from DesignatedContact

--- psm-app/services/src/main/java/gov/medicaid/entities/DesignatedContact.java
+++ psm-app/services/src/main/java/gov/medicaid/entities/DesignatedContact.java
@@ -17,12 +17,6 @@ package gov.medicaid.entities;
 
 import java.util.Date;
 
-/**
- * Represents a designated contact.
- *
- * @author TCSASSEMBLER
- * @version 1.0
- */
 public class DesignatedContact extends IdentifiableEntity {
 
     /**
@@ -35,9 +29,6 @@ public class DesignatedContact extends IdentifiableEntity {
      */
     private long ticketId;
 
-    /**
-     * The contact type.
-     */
     private DesignatedContactType type;
 
     /**
@@ -61,108 +52,50 @@ public class DesignatedContact extends IdentifiableEntity {
     public DesignatedContact() {
     }
 
-    /**
-     * Gets the value of the field <code>profileId</code>.
-     *
-     * @return the profileId
-     */
     public long getProfileId() {
         return profileId;
     }
 
-    /**
-     * Sets the value of the field <code>profileId</code>.
-     *
-     * @param profileId the profileId to set
-     */
     public void setProfileId(long profileId) {
         this.profileId = profileId;
     }
 
-    /**
-     * Gets the value of the field <code>ticketId</code>.
-     *
-     * @return the ticketId
-     */
     public long getTicketId() {
         return ticketId;
     }
 
-    /**
-     * Sets the value of the field <code>ticketId</code>.
-     *
-     * @param ticketId the ticketId to set
-     */
     public void setTicketId(long ticketId) {
         this.ticketId = ticketId;
     }
 
-    /**
-     * Gets the value of the field <code>type</code>.
-     *
-     * @return the type
-     */
     public DesignatedContactType getType() {
         return type;
     }
 
-    /**
-     * Sets the value of the field <code>type</code>.
-     *
-     * @param type the type to set
-     */
     public void setType(DesignatedContactType type) {
         this.type = type;
     }
 
-    /**
-     * Gets the value of the field <code>person</code>.
-     *
-     * @return the person
-     */
     public Person getPerson() {
         return person;
     }
 
-    /**
-     * Sets the value of the field <code>person</code>.
-     *
-     * @param person the person to set
-     */
     public void setPerson(Person person) {
         this.person = person;
     }
 
-    /**
-     * Gets the value of the field <code>sameAsProvider</code>.
-     *
-     * @return the sameAsProvider
-     */
     public String getSameAsProvider() {
         return sameAsProvider;
     }
 
-    /**
-     * Sets the value of the field <code>sameAsProvider</code>.
-     *
-     * @param sameAsProvider the sameAsProvider to set
-     */
     public void setSameAsProvider(String sameAsProvider) {
         this.sameAsProvider = sameAsProvider;
     }
 
-    /**
-     * Gets the value of the field <code>hireDate</code>.
-     * @return the hireDate
-     */
     public Date getHireDate() {
         return hireDate;
     }
 
-    /**
-     * Sets the value of the field <code>hireDate</code>.
-     * @param hireDate the hireDate to set
-     */
     public void setHireDate(Date hireDate) {
         this.hireDate = hireDate;
     }

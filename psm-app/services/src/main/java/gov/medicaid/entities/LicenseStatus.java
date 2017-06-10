705fd61fc1365bab2f55c2a1970b96ccab0ea783
Jason Owen <jason@jasonaowen.net>
2017-06-10 09:29:13 -0400

    Remove unused date field from LicenseStatus
    `LicenseStatus` is a lookup entity, meaning it is designed to have
    reference entries that other entities use. The `date` field, which was
    never accessed (as confirmed by both `git grep` and IntelliJ's Find
    Usages command), doesn't make sense for what is effectively a
    database-backed enum.
    
    Issue #101 License Status has an apparently unused property

--- psm-app/services/src/main/java/gov/medicaid/entities/LicenseStatus.java
+++ psm-app/services/src/main/java/gov/medicaid/entities/LicenseStatus.java
@@ -24,31 +24,9 @@ import java.util.Date;
  * @version 1.0
  */
 public class LicenseStatus extends LookupEntity {
-
-    /**
-     * License status date.
-     */
-    private Date date;
-
     /**
      * Default empty constructor.
      */
     public LicenseStatus() {
-    }
-
-    /**
-     * Gets the value of the field <code>date</code>.
-     * @return the date
-     */
-    public Date getDate() {
-        return date;
-    }
-
-    /**
-     * Sets the value of the field <code>date</code>.
-     * @param date the date to set
-     */
-    public void setDate(Date date) {
-        this.date = date;
     }
 }

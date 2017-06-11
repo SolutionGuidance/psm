SolutionGuidance/psm

commit 322664d4192e238d6960a62ab7022126a350086f

Annotate Entity, Person, and Organization
`Person` and `Organization` extend `Entity`. Previously, this was
expressed in the database as a single table (`ENTITY`) with a
discriminator field; Hibernate would read that field and construct the
appropriate object (`Person` or `Organization`). This can have
performance benefits over the alternatives, but I find it makes the
schema a little harder to understand.

In migrating these for Hibernate 5, separate them into three distinct
tables using the joined inheritance strategy. The `people` and
`organizations` tables share a primary key with the `entities` table,
and add their subtype-specific fields. Hibernate figures out which kind
of `Entity` an entity is by joining against those two tables (that is,
we are omitting the optional discriminator column - if that turns out to
be a performance penalty we care about, we can add it after profiling).

See also: https://en.wikibooks.org/wiki/Java_Persistence/Inheritance

Issue #36 Use Hibernate 5, instead of 4

psm-app/db/seed.sql
@@ -12,8 +12,11 @@ DROP TABLE IF EXISTS
   degrees,
   enrollment_statuses,
   enrollments,
+  entities,
   help_items,
   license_types,
+  organizations,
+  people,
   persistent_logins,
   profile_statuses,
   provider_profiles,
@@ -603,3 +606,59 @@ INSERT INTO provider_type_license_types(
   ('68', 'AO'),
   ('68', 'AZ'),
   ('69', 'AO');
+
+CREATE TABLE entities(
+  entity_id BIGINT PRIMARY KEY,
+  enrolled CHARACTER VARYING(1) NOT NULL,
+  profile_id BIGINT NOT NULL,
+  ticket_id BIGINT,
+  name TEXT,
+  legal_name TEXT,
+  legacy_indicator CHARACTER VARYING(1),
+  legacy_id TEXT,
+  npi TEXT,
+  npi_verified CHARACTER VARYING(1),
+  npi_lookup_verified CHARACTER VARYING(1),
+  nonexclusion_verified CHARACTER VARYING(1),
+  provider_type_code CHARACTER VARYING(2)
+    REFERENCES provider_types(code),
+  provider_sub_type TEXT,
+  contact_id BIGINT
+    REFERENCES contacts(contact_id),
+  background_study_id TEXT,
+  background_clearance_date TIMESTAMP WITH TIME ZONE
+);
+CREATE TABLE organizations(
+  entity_id BIGINT PRIMARY KEY
+    REFERENCES entities(entity_id),
+  fein CHARACTER VARYING(10),
+  agency_id TEXT,
+  billing_same_as_primary CHARACTER VARYING(1),
+  reimbursement_same_as_primary CHARACTER VARYING(1),
+  ten99_same_as_primary CHARACTER VARYING(1),
+  billing_address_id BIGINT
+    REFERENCES addresses(address_id),
+  reimbursement_address_id BIGINT
+    REFERENCES addresses(address_id),
+  ten99_address_id BIGINT
+    REFERENCES addresses(address_id),
+  state_tax_id TEXT,
+  fiscal_year_end TEXT,
+  remittance_sequence_order TEXT,
+  eft_vendor_number TEXT
+);
+CREATE TABLE people(
+  entity_id BIGINT PRIMARY KEY
+    REFERENCES entities(entity_id),
+  prefix TEXT,
+  first_name TEXT,
+  middle_name TEXT,
+  last_name TEXT,
+  suffix TEXT,
+  ssn TEXT,
+  ssn_verified CHARACTER VARYING(1),
+  birth_date DATE,
+  degree_code CHARACTER VARYING(2)
+    REFERENCES degrees(code),
+  degree_award_date DATE
+);

psm-app/services/src/main/java/gov/medicaid/entities/Entity.java
@@ -15,6 +15,16 @@
  */
 package gov.medicaid.entities;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.io.Serializable;
 import java.util.Date;
 
 
@@ -24,21 +34,31 @@ import java.util.Date;
  * @author TCSASSEMBLER
  * @version 1.0
  */
-public abstract class Entity extends IdentifiableEntity {
+@javax.persistence.Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@Table(name = "entities")
+public abstract class Entity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "entity_id")
+    private long id;
 
     /**
      * Enrolled flag.
      */
+    @Column(nullable = false)
     private String enrolled;
 
     /**
      * If enrolled, the profile identifier.
      */
+    @Column(name = "profile_id", nullable = false)
     private long profileId;
 
     /**
      * References the ticket for this request.
      */
+    @Column(name = "ticket_id")
     private long ticketId;
 
     /**
@@ -49,10 +69,13 @@ public abstract class Entity extends IdentifiableEntity {
     /**
      * The full legal name.
      */
+    @Column(name = "legal_name")
     private String legalName;
 
+    @Column(name = "legacy_indicator")
     private String legacyIndicator;
 
+    @Column(name = "legacy_id")
     private String legacyId;
 
     private String npi;
@@ -60,36 +83,54 @@ public abstract class Entity extends IdentifiableEntity {
     /**
      * NPI - PECOS.
      */
+    @Column(name = "npi_verified")
     private String npiVerifiedInd;
 
     /**
      * NPI - NPILookup.
      */
+    @Column(name = "npi_lookup_verified")
     private String npiLookupVerifiedInd;
 
+    @Column(name = "nonexclusion_verified")
     private String nonExclusionVerifiedInd;
 
     /**
      * The provider type (if enrolled).
      */
+    @ManyToOne
+    @JoinColumn(name = "provider_type_code")
     private ProviderType providerType;
 
     /**
      * Additional type information.
      */
+    @Column(name = "provider_sub_type")
     private String providerSubType;
 
+    @ManyToOne
+    @JoinColumn(name = "contact_id")
     private ContactInformation contactInformation;
 
     /**
      * For agencies.
      */
+    @Column(name = "background_study_id")
     private String backgroundStudyId;
 
     /**
      * For agencies.
      */
+    @Column(name = "background_clearance_date")
     private Date backgroundClearanceDate;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;

psm-app/cms-business-process/src/main/resources/META-INF/Medicaid.hbm.xml
@@ -2,169 +2,6 @@
 <!DOCTYPE hibernate-mapping PUBLIC "-//Hibernate/Hibernate Mapping DTD 3.0//EN"
                                    "http://hibernate.sourceforge.net/hibernate-mapping-3.0.dtd">
 <hibernate-mapping default-lazy="false">
-	<class abstract="true" name="gov.medicaid.entities.Entity" table="ENTITY">
-		<id name="id" type="long">
-			<column name="ENTITY_ID" />
-			<generator class="assigned" />
-		</id>
-		<discriminator force="false" insert="true" not-null="true">
-			<column length="1" name="PERSON_IND" />
-		</discriminator>
-		<property generated="never" lazy="false" name="enrolled"
-			not-null="true" type="string">
-			<column length="1" name="ENROLLED_IND" />
-		</property>
-		<property generated="never" lazy="false" name="profileId"
-			not-null="true" type="long">
-			<column name="PROFILE_ID" />
-		</property>
-		<property generated="never" lazy="false" name="ticketId"
-			not-null="true" type="long">
-			<column name="TICKET_ID" />
-		</property>
-		<property generated="never" lazy="false" name="name" type="string">
-			<column length="100" name="NAME" />
-		</property>
-		<property generated="never" lazy="false" name="legalName"
-			type="string">
-			<column length="100" name="LEGAL_NAME" />
-		</property>
-		<property generated="never" lazy="false" name="legacyIndicator"
-			type="string">
-			<column length="1" name="LEGACY_IND" />
-		</property>
-		<property generated="never" lazy="false" name="legacyId"
-			type="string">
-			<column length="9" name="LEGACY_ID" />
-		</property>
-		<property generated="never" lazy="false" name="npi" type="string">
-			<column length="10" name="NPI" />
-		</property>
-		<property generated="never" lazy="false" name="npiVerifiedInd"
-			type="string">
-			<column length="1" name="NPI_VERIFIED_IND" />
-		</property>
-		<property generated="never" lazy="false" name="npiLookupVerifiedInd"
-			type="string">
-			<column length="1" name="NPI_LU_VERIFIED_IND" />
-		</property>
-		<property generated="never" lazy="false" name="nonExclusionVerifiedInd"
-			type="string">
-			<column length="1" name="NONEXCL_VERIFIED_IND" />
-		</property>
-		<property generated="never" lazy="false" name="backgroundStudyId"
-			type="string">
-			<column length="100" name="BGS_ID" />
-		</property>
-		<property generated="never" lazy="false" name="backgroundClearanceDate"
-			type="date">
-			<column name="BGS_CLEARANCE_DT" />
-		</property>
-		<property generated="never" lazy="false" name="providerSubType"
-			type="string">
-			<column name="SUB_TYP" />
-		</property>
-		<many-to-one class="gov.medicaid.entities.ProviderType"
-			fetch="join" lazy="false" name="providerType">
-			<column name="PROVIDER_TYP_CD" />
-		</many-to-one>
-		<many-to-one class="gov.medicaid.entities.ContactInformation"
-			fetch="join" lazy="false" name="contactInformation">
-			<column name="PRIMARY_CONTACT_ID" />
-		</many-to-one>
-	</class>
-	<subclass discriminator-value="Y" extends="gov.medicaid.entities.Entity"
-		name="gov.medicaid.entities.Person" select-before-update="false">
-		<property generated="never" lazy="false" name="middleName"
-			type="string">
-			<column length="50" name="MIDDLE_NAME" />
-		</property>
-		<property generated="never" lazy="false" name="ssn" type="string">
-			<column length="9" name="SSN" />
-		</property>
-		<property generated="never" lazy="false" name="ssnVerifiedInd"
-			type="string">
-			<column length="1" name="SSN_VERIFIED_IND" />
-		</property>
-		<property generated="never" lazy="false" name="dob" type="date">
-			<column name="BIRTH_DT" />
-		</property>
-		<many-to-one class="gov.medicaid.entities.Degree" fetch="join"
-			name="degree">
-			<column name="DEGREE_CD" />
-		</many-to-one>
-		<property generated="never" lazy="false" name="degreeAwardDate"
-			type="date">
-			<column name="DEGREE_AWARD_DT" />
-		</property>
-		<property generated="never" lazy="false" name="prefix" type="string">
-			<column length="50" name="PREFIX" />
-		</property>
-		<property generated="never" lazy="false" name="suffix" type="string">
-			<column length="50" name="SUFFIX" />
-		</property>
-		<property generated="never" lazy="false" name="firstName"
-			type="string">
-			<column length="50" name="FIRST_NAME" />
-		</property>
-		<property generated="never" lazy="false" name="lastName"
-			type="string">
-			<column length="50" name="LAST_NAME" />
-		</property>
-	</subclass>
-	<subclass discriminator-value="N" extends="gov.medicaid.entities.Entity"
-		name="gov.medicaid.entities.Organization" select-before-update="false">
-		<property generated="never" lazy="false" name="stateTaxId"
-			type="string">
-			<column length="10" name="STATE_TAX_ID" />
-		</property>
-		<property generated="never" lazy="false" name="fein" type="string">
-			<column length="7" name="FEIN" />
-		</property>
-		<property generated="never" lazy="false" name="agencyId"
-			type="string">
-			<column length="100" name="AGENCY_ID" />
-		</property>
-		<property generated="never" lazy="false" name="billingSameAsPrimary"
-			type="string">
-			<column length="1" name="SAME_BILLING_ADDRESS_IND" />
-		</property>
-		<property generated="never" lazy="false" name="reimbursementSameAsPrimary"
-			type="string">
-			<column length="1" name="SAME_REIMBURSEMENT_ADDRESS_IND" />
-		</property>
-		<property generated="never" lazy="false" name="ten99SameAsPrimary"
-			type="string">
-			<column length="1" name="SAME_1099_ADDRESS_IND" />
-		</property>
-		<many-to-one class="gov.medicaid.entities.Address" fetch="join"
-			name="billingAddress">
-			<column name="BILLING_ADDRESS_ID" />
-		</many-to-one>
-		<many-to-one class="gov.medicaid.entities.Address" fetch="join"
-			name="reimbursementAddress">
-			<column name="REIMBURSEMENT_ADDRESS_ID" />
-		</many-to-one>
-		<many-to-one class="gov.medicaid.entities.Address" fetch="join"
-			name="ten99Address">
-			<column name="TEN99_ADDRESS_ID" />
-		</many-to-one>
-		<property generated="never" lazy="false" name="fiscalYearEnd"
-			type="string">
-			<column length="5" name="FISCAL_YEAR_END" />
-		</property>
-		<property generated="never" lazy="false" name="remittanceSequenceOrder">
-			<column name="REMITTANCE_SEQUENCE_ORDER" />
-			<type name="org.hibernate.type.EnumType">
-				<param name="type">12</param>
-				<param name="enumClass">gov.medicaid.entities.RemittanceSequenceOrder</param>
-			</type>
-		</property>
-		<property generated="never" lazy="false" name="eftVendorNumber"
-			type="string">
-			<column length="14" name="EFT_VENDOR_NUMBER" />
-		</property>
-	</subclass>
 	<class name="gov.medicaid.entities.Affiliation" table="PROFILE_GRP_XREF">
 		<id name="id" type="long">
 			<column name="PROFILE_GRP_XREF_ID" />

psm-app/cms-business-process/src/main/resources/META-INF/persistence.xml
@@ -51,8 +51,11 @@
     <class>gov.medicaid.entities.Degree</class>
     <class>gov.medicaid.entities.Enrollment</class>
     <class>gov.medicaid.entities.EnrollmentStatus</class>
+    <class>gov.medicaid.entities.Entity</class>
     <class>gov.medicaid.entities.HelpItem</class>
     <class>gov.medicaid.entities.LicenseType</class>
+    <class>gov.medicaid.entities.Organization</class>
+    <class>gov.medicaid.entities.Person</class>
     <class>gov.medicaid.entities.ProfileStatus</class>
     <class>gov.medicaid.entities.ProviderProfile</class>
     <class>gov.medicaid.entities.ProviderType</class>

psm-app/services/src/main/java/gov/medicaid/entities/Organization.java
@@ -15,60 +15,88 @@
  */
 package gov.medicaid.entities;
 
+import javax.persistence.Column;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@javax.persistence.Entity
+@Table(name = "organizations")
 public class Organization extends Entity {
 
     /**
-     * Employer number.
+     * The Federal Employer Identification Number (FEIN) is a unique nine-digit
+     * number written in the form 00-0000000.
+     *
+     * @see <a href="https://en.wikipedia.org/wiki/Employer_Identification_Number">
+     * Wikipedia: Employer Identification Number</a>
      */
     private String fein;
 
     /**
      * For agencies.
      */
+    @Column(name = "agency_id")
     private String agencyId;
 
     /**
      * Indicates that the billing address is the same as the primary address.
      */
+    @Column(name = "billing_same_as_primary")
     private String billingSameAsPrimary;
 
     /**
      * Indicates that the reimbursement address is the same as the primary address.
      */
+    @Column(name = "reimbursement_same_as_primary")
     private String reimbursementSameAsPrimary;
 
     /**
      * Indicates that the ten99 address is the same as the primary address.
      */
+    @Column(name = "ten99_same_as_primary")
     private String ten99SameAsPrimary;
 
     /**
      * Practice secondary address.
      */
+    @ManyToOne
+    @JoinColumn(name = "billing_address_id")
     private Address billingAddress;
 
     /**
      * Practice secondary address.
      */
+    @ManyToOne
+    @JoinColumn(name = "reimbursement_address_id")
     private Address reimbursementAddress;
 
     /**
      * Practice secondary address.
      */
+    @ManyToOne
+    @JoinColumn(name = "ten99_address_id")
     private Address ten99Address;
 
     /**
      * Practice minnesotaTaxId.
      */
+    @Column(name = "state_tax_id")
     private String stateTaxId;
 
     /**
      * Practice fiscal year end.
      */
+    @Column(name = "fiscal_year_end")
     private String fiscalYearEnd;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "remittance_sequence_order")
     private RemittanceSequenceOrder remittanceSequenceOrder;
 
+    @Column(name = "eft_vendor_number")
     private String eftVendorNumber;
 
     /**

psm-app/services/src/main/java/gov/medicaid/entities/Person.java
@@ -15,33 +15,44 @@
  */
 package gov.medicaid.entities;
 
+import javax.persistence.Column;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.util.Date;
 
+@javax.persistence.Entity
+@Table(name = "people")
 public class Person extends Entity {
     private String ssn;
 
     /**
      * SSN Verification status.
      */
+    @Column(name = "ssn_verified")
     private String ssnVerifiedInd;
 
-    /**
-     * Date of birth.
-     */
+    @Column(name = "birth_date")
     private Date dob;
 
+    @ManyToOne
+    @JoinColumn(name = "degree_code")
     private Degree degree;
 
+    @Column(name = "degree_award_date")
     private Date degreeAwardDate;
 
     private String prefix;
 
     private String suffix;
 
+    @Column(name = "first_name")
     private String firstName;
 
+    @Column(name = "last_name")
     private String lastName;
 
+    @Column(name = "middle_name")
     private String middleName;
 
     public String getSsn() {

commit baa572ca66c037bffb71113a6b10fbb6c0cf1376

Allow null enrollment status on Entity
The Hibernate XML mapping file said the `enrolled` field must be backed
by a non-nullable column, but looking at the source, there is no default
and it is only ever written to with the string 'Y'. Looking at the
Oracle database instance, it appears that the table was created with a
nullable column. Do the same.

We should fix this, long-term; this appears to be another artifact of
Oracle not having a boolean data type. See issue #57.

Bug introduced in 322664d4192e238d6960a62ab7022126a350086f.

Issue #36 Use Hibernate 5, instead of 4

psm-app/db/seed.sql
@@ -610,7 +610,7 @@ INSERT INTO provider_type_license_types(
 
 CREATE TABLE entities(
   entity_id BIGINT PRIMARY KEY,
-  enrolled CHARACTER VARYING(1) NOT NULL,
+  enrolled CHARACTER VARYING(1),
   profile_id BIGINT NOT NULL,
   ticket_id BIGINT,
   name TEXT,

psm-app/services/src/main/java/gov/medicaid/entities/Entity.java
@@ -46,7 +46,6 @@ public abstract class Entity implements Serializable {
     /**
      * Enrolled flag.
      */
-    @Column(nullable = false)
     private String enrolled;
 
     /**
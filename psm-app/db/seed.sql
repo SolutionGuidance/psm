8b0b188a24d63e1fb068240f2d3ab3c449285299
Jason Owen <jason@jasonaowen.net>
2017-07-03 13:05:47 -0400

    Remove service assurance type
    The entity was only referenced by now-deleted code, so we may delete it.
    Also delete the XML-defined and apparently-never-used type of the same
    name.
    
    Issue #36 Use Hibernate 5, instead of 4
    Pull request #72 Convert ServiceAssuranceType to annotation

--- psm-app/db/seed.sql
+++ psm-app/db/seed.sql
@@ -55,7 +55,6 @@ DROP TABLE IF EXISTS
   risk_levels,
   roles,
   screening_schedules,
-  service_assurance_types,
   service_categories,
   specialty_types,
   states
@@ -79,12 +78,6 @@ INSERT INTO roles (code, description) VALUES
   ('R2', 'Service Agent'),
   ('R3', 'Service Administrator'),
   ('R4', 'System Administrator');
-
-CREATE TABLE service_assurance_types (
-  code CHARACTER VARYING(2) PRIMARY KEY,
-  description TEXT UNIQUE,
-  patient_indicator TEXT
-);
 
 CREATE TABLE cms_user (
   user_id TEXT PRIMARY KEY,

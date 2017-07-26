15fdab5c5f5caad2c1b35421ed70c726c57395c3
Jason Owen <jason@jasonaowen.net>
2017-07-26 15:49:32 -0400

    Optimize imports in exclusion handler

--- psm-app/cms-business-process/src/main/java/gov/medicaid/process/enrollment/ExcludedProvidersScreeningHandler.java
+++ psm-app/cms-business-process/src/main/java/gov/medicaid/process/enrollment/ExcludedProvidersScreeningHandler.java
@@ -15,6 +15,8 @@
  */
 package gov.medicaid.process.enrollment;
 
+import com.topcoder.util.log.Level;
+import com.topcoder.util.log.Log;
 import gov.medicaid.binders.XMLUtility;
 import gov.medicaid.domain.model.EnrollmentProcess;
 import gov.medicaid.domain.model.ExternalSourcesScreeningResultType;
@@ -26,17 +28,12 @@ import gov.medicaid.domain.rules.inference.MatchStatus;
 import gov.medicaid.services.util.LogUtil;
 import gov.medicaid.verification.OIGExclusionSearchClient;
 import gov.medicaid.verification.OIGExclusionServiceMatcher;
-
-import java.io.IOException;
-
-import javax.xml.bind.JAXBException;
-import javax.xml.transform.TransformerException;
-
 import org.drools.runtime.process.WorkItem;
 import org.drools.runtime.process.WorkItemManager;
 
-import com.topcoder.util.log.Level;
-import com.topcoder.util.log.Log;
+import javax.xml.bind.JAXBException;
+import javax.xml.transform.TransformerException;
+import java.io.IOException;
 
 /**
  * This checks the excluded providers from the OIG website.

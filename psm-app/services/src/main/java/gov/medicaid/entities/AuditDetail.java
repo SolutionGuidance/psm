9ad3daee05e0c22ff0ef97d0b04e0221b8a18269
Jason Owen <jason@jasonaowen.net>
2017-07-28 17:49:17 -0400

    Remove unused import from AuditDetail

--- psm-app/services/src/main/java/gov/medicaid/entities/AuditDetail.java
+++ psm-app/services/src/main/java/gov/medicaid/entities/AuditDetail.java
@@ -19,7 +19,6 @@ import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 

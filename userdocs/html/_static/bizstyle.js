64d3c3d312c332a5869c94d09a371b2bb1b55464
Sumana Harihareswara <sh@changeset.nyc>
2017-10-11 11:33:00 -0400

    Publish user docs
    Generated on 2017-10-11:11:32:53, pushed to gh-pages

--- userdocs/html/_static/bizstyle.js
+++ userdocs/html/_static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("Provider Screening Module 1.0 documentation");
+        $("li.nav-item-0 a").text("Provider Screening Module user manual 1.0 documentation");
     }
 });

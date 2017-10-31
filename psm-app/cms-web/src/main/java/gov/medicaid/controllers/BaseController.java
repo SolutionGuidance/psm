SolutionGuidance/psm

commit 31405615e8b1fcdbf3546cafaf03dfceeebaf9d6

Add handlebars template variables to all requests
Create a new interceptor that adds the variables that handlebars
templates need. This allows us to stop manually adding these variables
via a call to a helper method; move the contents of that helper method
to the interceptor.

Also update the handlebars custom tag to include variables from all
scopes, rather than only PAGE_SCOPE, as adding variables in an
interceptor seems to add them to the REQUEST_SCOPE.

Remove the common variables added for handlebars templates (mainly
`ctx`), as they are now set once rather than needing to be set every
time a handlebars template is invoked.

Some of the variables are based on the principal user, an attribute of a
request set by spring security. Since we previously were only accessing
the principal on logged-in pages, we now need to handle the case of
anonymous access (allowed, for example, on the login page). Update the
`getPrincipal` helper to account for anonymous access.

Issue #238 Templatize UI

psm-app/cms-web/src/main/java/gov/medicaid/interceptors/HandlebarsInterceptor.java
@@ -1,0 +1,62 @@
+package gov.medicaid.interceptors;
+
+import gov.medicaid.controllers.ControllerHelper;
+import gov.medicaid.entities.CMSUser;
+import gov.medicaid.security.CMSPrincipal;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+public class HandlebarsInterceptor extends HandlerInterceptorAdapter {
+    @Override
+    public void postHandle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Object handler,
+            ModelAndView modelAndView
+    ) throws Exception {
+        if (modelAndView == null ||
+                modelAndView.getViewName().startsWith("redirect:")) {
+            return;
+        }
+
+        // <c:set var="ctx" value="${pageContext.request.contextPath}"/>
+        modelAndView.addObject("ctx", request.getContextPath());
+
+        CMSPrincipal principal = ControllerHelper.getPrincipal();
+        if (principal != null) {
+            CMSUser principalUser = principal.getUser();
+            modelAndView.addObject("principalUser", principalUser);
+
+            // <c:if test="${requestPrincipal.user.role.description eq 'Service Administrator'}">
+            if ("Service Administrator".equals(principalUser.getRole().getDescription())) {
+                modelAndView.addObject(
+                        "isServiceAdministrator",
+                        true
+                );
+            }
+
+            // <sec:authentication property="principal.loginDate" var="loginDate"/>
+            // <fmt:setLocale value="en_US" scope="session"/>
+            // Last login: <fmt:formatDate value="${loginDate}" pattern="EEEE, d MMMM yyyy hh:mm:ss a zzz"/>
+            SimpleDateFormat dateFormat = new SimpleDateFormat(
+                    "EEEE, d MMMM yyyy hh:mm:ss a zzz",
+                    Locale.US
+            );
+            modelAndView.addObject(
+                    "loginDate",
+                    dateFormat.format(principal.getLoginDate())
+            );
+
+            // TODO for provider/profile/list template of MyProfileController
+            // <sec:authentication property="principal.authenticatedBySystem" var="authenticatedBySystem"/>
+            // <sec:authentication property="principal" var="requestPrincipal"/>
+            // <spring:eval expression="authenticatedBySystem == T(gov.medicaid.entities.SystemId).CMS_ONLINE" var="isInternalUser" />
+            // if (principal.authenticatedBySystem == gov.medicaid.entities.SystemId.CMS_ONLINE) model.addObject("isInternalUser", true);
+        }
+    }
+}

psm-app/cms-web/WebContent/WEB-INF/mvc-dispatcher-servlet.xml
@@ -30,6 +30,10 @@
       <mvc:mapping path="/**"/>
       <bean class="gov.medicaid.interceptors.FlashMessageInterceptor"/>
     </mvc:interceptor>
+    <mvc:interceptor>
+      <mvc:mapping path="/**"/>
+      <bean class="gov.medicaid.interceptors.HandlebarsInterceptor"/>
+    </mvc:interceptor>
   </mvc:interceptors>
 
   <bean id="log4jLogFactory"

psm-app/cms-web/WebContent/WEB-INF/pages/accounts/register.jsp
@@ -5,7 +5,6 @@
 <%@ taglib prefix="h" tagdir="/WEB-INF/tags" %>
 <html xmlns="http://www.w3.org/1999/xhtml">
   <c:set var="title" value="Register"/>
-  <c:set var="ctx" value="${pageContext.request.contextPath}"/>
   <h:handlebars template="includes/html_head" context="${pageContext}"/>
   <body>
     <div id="wrapper">

psm-app/cms-web/WebContent/WEB-INF/pages/accounts/register_success.jsp
@@ -5,7 +5,6 @@
 <%@ taglib prefix="h" tagdir="/WEB-INF/tags" %>
 <html xmlns="http://www.w3.org/1999/xhtml">
   <c:set var="title" value="Registration Complete"/>
-  <c:set var="ctx" value="${pageContext.request.contextPath}"/>
   <h:handlebars template="includes/html_head" context="${pageContext}"/>
   <body>
     <div id="wrapper">

psm-app/cms-web/WebContent/WEB-INF/pages/admin/includes/header.jsp
@@ -66,10 +66,7 @@
           </div>
           <!-- /.userSection -->
           <div class="mastHead">
-            <sec:authentication property="principal.loginDate" var="loginDate"/>
-            Last login:
-            <fmt:formatDate value="${loginDate}"
-                            pattern="EEEE, d MMMM yyyy hh:mm:ss a zzz"/>
+            Last login: <c:out value="${loginDate}" />
           </div>
           <!-- /.mastHead -->
         </div>

psm-app/cms-web/WebContent/WEB-INF/pages/admin/includes/taglibs.jsp
@@ -14,5 +14,4 @@
 <%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
 <%@ taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
 <%@ taglib prefix="h" tagdir="/WEB-INF/tags" %>
-<c:set var="ctx" value="${pageContext.request.contextPath}"/>
 <fmt:setLocale value="en_US" scope="session"/>

psm-app/cms-web/WebContent/WEB-INF/pages/error.jsp
@@ -10,7 +10,6 @@
   <c:when test="${principal ne null && principal.enabled}">
     <html xmlns="http://www.w3.org/1999/xhtml">
       <c:set var="title" value="Server Error"/>
-      <c:set var="ctx" value="${pageContext.request.contextPath}"/>
       <h:handlebars template="includes/html_head" context="${pageContext}"/>
       <body>
         <div id="wrapper">

psm-app/cms-web/WebContent/WEB-INF/pages/forgot_password.jsp
@@ -5,7 +5,6 @@
 <%@ taglib prefix="h" tagdir="/WEB-INF/tags" %>
 <html xmlns="http://www.w3.org/1999/xhtml">
   <c:set var="title" value="Forgot Password"/>
-  <c:set var="ctx" value="${pageContext.request.contextPath}"/>
   <h:handlebars template="includes/html_head" context="${pageContext}"/>
   <body>
     <div id="wrapper">

commit 1e7c61c0d1c184668acf42110bace8c56edc8d1f

Add common variables to error pages
The interceptor introduced in 31405615e8b1fcdbf3546cafaf03dfceeebaf9d6
does not get called for unsuccessful requests, which seems to be as
intended[1]. Manually invoke it in our exception handlers to make sure
that static assets can be loaded from the error page.

[1] http://forum.spring.io/forum/spring-projects/web/114511-interceptor-posthandle-skipped-on-exception

Issue #504 Stack trace when mail server unavailable

psm-app/cms-web/src/main/java/gov/medicaid/controllers/BaseController.java
@@ -18,13 +18,16 @@ package gov.medicaid.controllers;
 
 import com.topcoder.util.log.Log;
 import gov.medicaid.controllers.validators.StrictCustomDateEditor;
+import gov.medicaid.interceptors.HandlebarsInterceptor;
 import gov.medicaid.services.PortalServiceConfigurationException;
 import gov.medicaid.services.util.LogUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Date;
 
@@ -77,15 +80,22 @@ public abstract class BaseController {
     }
 
     /**
-     * Captures any exception that is thrown from the controllers and sends the user to the
-     * error page.
-     * @param ex the exception thrown
+     * Captures any exception that is thrown from the controllers and renders
+     * the error page.
+     *
+     * @param request the request that resulted in an exception
+     * @param ex      the exception thrown
      * @return the error view
      */
     @ExceptionHandler(Exception.class)
-    public String handleError(Exception ex) {
+    public ModelAndView handleError(
+            HttpServletRequest request,
+            Exception ex
+    ) {
         LogUtil.traceError(getLog(), "BaseController#handleError(Exception ex)", ex);
-        return "error";
+        ModelAndView view = new ModelAndView("error");
+        HandlebarsInterceptor.addCommonVariables(request, view);
+        return view;
     }
 
     /**

psm-app/cms-web/src/main/java/gov/medicaid/controllers/admin/BaseSystemAdminController.java
@@ -17,6 +17,7 @@
 package gov.medicaid.controllers.admin;
 
 import com.topcoder.util.log.Log;
+import gov.medicaid.interceptors.HandlebarsInterceptor;
 import gov.medicaid.services.LookupService;
 import gov.medicaid.services.PortalServiceConfigurationException;
 import gov.medicaid.services.RegistrationService;
@@ -24,6 +25,8 @@ import gov.medicaid.services.util.LogUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * A base controller class that other classes will extend that provides logging, auditing, and additional services.
@@ -65,14 +68,20 @@ public abstract class BaseSystemAdminController {
 
     /**
      * Directs all exceptions encountered by subclasses to a generic error page.
-     * @param ex the exception encountered
+     *
+     * @param request the request that resulted in an exception
+     * @param ex      the exception encountered
      * @return the error view name
      */
     @ExceptionHandler(Exception.class)
-    public ModelAndView handleException(Exception ex) {
+    public ModelAndView handleException(
+            HttpServletRequest request,
+            Exception ex
+    ) {
         LogUtil.traceError(getLog(), "BaseSystemAdminController#handleException(Exception ex)", ex);
         ModelAndView view = new ModelAndView("error");
         view.addObject("exception", ex);
+        HandlebarsInterceptor.addCommonVariables(request, view);
         return view;
     }
 

psm-app/cms-web/src/main/java/gov/medicaid/interceptors/HandlebarsInterceptor.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.constraints.NotNull;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 
@@ -24,6 +25,13 @@ public class HandlebarsInterceptor extends HandlerInterceptorAdapter {
             return;
         }
 
+        addCommonVariables(request, modelAndView);
+    }
+
+    public static void addCommonVariables(
+            @NotNull HttpServletRequest request,
+            @NotNull ModelAndView modelAndView
+    ) {
         // <c:set var="ctx" value="${pageContext.request.contextPath}"/>
         modelAndView.addObject("ctx", request.getContextPath());
 
1635efda873e5429ab9900cec561ee94aac69b8e
cecilia-donnelly <cdonnelly@opentechstrategies.com>
2018-01-04 17:27:02 -0600

    Separate steps for full Serenity features: #568
    Putting methods in a @Step library means that Serenity will take
    screenshots of test execution at each of those steps.  These screenshots
    can then be used in later reporting.  See
    https://chat.opentechstrategies.com/#narrow/stream/Provider.20Screening/topic/Selenium
    for a discussion of why we decided to follow this pattern.

--- psm-app/integration-tests/src/test/java/gov/medicaid/features/general/steps/ChangePasswordStepDefinitions.java
+++ psm-app/integration-tests/src/test/java/gov/medicaid/features/general/steps/ChangePasswordStepDefinitions.java
@@ -5,29 +5,23 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import net.thucydides.core.annotations.Steps;
 
-import gov.medicaid.features.general.ui.DashboardPage;
-import gov.medicaid.features.general.ui.MyProfilePage;
-
 @SuppressWarnings("unused")
 public class ChangePasswordStepDefinitions {
     @Steps
     GeneralSteps generalSteps;
 
-    private DashboardPage dashboardPage;
-    private MyProfilePage profilePage;
-
     @Given("^I am on the dashboard page$")
     public void check_dashboard_page() {
-        dashboardPage.checkOnDashboard();
+        generalSteps.checkOnDashboard();
     }
 
     @When("^I click on My Profile$")
     public void i_click_on_my_profile()  {
-        dashboardPage.clickMyProfile();
+        generalSteps.clickMyProfile();
     }
 
     @Then("^I should see the Change Password link$")
     public void i_should_see_change_password()  {
-        profilePage.checkChangePassword();
+        generalSteps.seeChangePassword();
     }
 }

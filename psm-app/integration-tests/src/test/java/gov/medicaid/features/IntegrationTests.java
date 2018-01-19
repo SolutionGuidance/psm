d95cee3868fdd02d6c721a03b411f05cfbd8c6e5
Paul Morris <pwm@paulwmorris.com>
2018-01-19 12:06:38 -0500

    Add axe-selenium dependency and test Login page a11y (WIP)
    Issue #518 Implement accessibility checking in CI

--- psm-app/integration-tests/src/test/java/gov/medicaid/features/IntegrationTests.java
+++ psm-app/integration-tests/src/test/java/gov/medicaid/features/IntegrationTests.java
@@ -1,13 +1,21 @@
 package gov.medicaid.features;
 
+import com.deque.axe.AXE;
 import cucumber.api.CucumberOptions;
 import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 /**
@@ -28,5 +36,30 @@ public class IntegrationTests {
     public static void click(PageObject pageObject, WebElement target) {
         pageObject.evaluateJavascript("arguments[0].scrollIntoView()", target);
         pageObject.clickOn(target);
+    }
+
+    public static void testAccessibility(PageObject pageObject) {
+
+        // Temporarily hard-coded URL, needs work
+        URL axeCoreUrl = null;
+        try {
+            axeCoreUrl = new URL("http://localhost:8080/cms/js/node_modules/axe-core/axe.min.js");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        WebDriver driver = pageObject.getDriver();
+
+        JSONObject responseJSON = new AXE.Builder(driver, axeCoreUrl)
+                .options("{ runOnly: { type: \"tag\", values: [\"wcag2aa\"] } }")
+                .analyze();
+
+        JSONArray violations = responseJSON.getJSONArray("violations");
+
+        if (violations.length() > 0) {
+            AXE.writeResults("accessibility-violations-" + pageObject.getTitle(), violations);
+        }
+
+        assertThat(violations.length()).isEqualTo(0);
     }
 }

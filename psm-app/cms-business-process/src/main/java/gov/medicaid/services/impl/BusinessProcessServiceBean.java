5c8028807f567ec205bcebd7443436fd47f7723f
Jason Owen <jason@jasonaowen.net>
2018-04-10 17:50:11 -0400

    Remove empty SIRS process step
    According to the Minnesota Department of Human Services[1], it has
    
        ...an area responsible to look for and investigate suspected fraud.
        The Surveillance and Integrity Review Section (SIRS):
    
        - Conducts post-payment review of claims to determine the accuracy
          of the payment
        - Investigates reports received on the SIRS Hotline
        - Measures performance and quality of services provided by DHS
    
    They may have a role to play in reviewing provider enrollment
    applications in Minnesota, but if so, this was never fully implemented
    in the PSM.
    
    Remove this Minnesota-specific, partially-implemented step in our
    business process, along with the supporting rules and data model
    attributes.
    
    [1] http://registrations.dhs.state.mn.us/PCACourse/Module_08/en/0205.htm
    
    Issue #165 Remove/edit code pointing to old external-sources app
    Issue #571 Remove MN-specific rules

--- psm-app/cms-business-process/src/main/java/gov/medicaid/services/impl/BusinessProcessServiceBean.java
+++ psm-app/cms-business-process/src/main/java/gov/medicaid/services/impl/BusinessProcessServiceBean.java
@@ -156,7 +156,6 @@ public class BusinessProcessServiceBean extends BaseService implements BusinessP
         handlers.put("Auto Disqualification", new DisqualificationHandler());
         handlers.put("Auto Screening", new ScreeningHandler());
         handlers.put("Background Check", new SystemOutWorkItemHandler());
-        handlers.put("SIRS", new SystemOutWorkItemHandler());
         handlers.put("Accept Application", new AcceptedHandler(notificationService));
 
         if (providerService == null) {

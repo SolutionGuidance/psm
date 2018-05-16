dd5dfb24fd9c963c321915f26051530b71aacf18
Frank Duncan <frank@kank.net>
2018-05-16 09:54:15 -0500

    Fix #815.  Use username for revewers report

--- psm-app/cms-web/src/main/java/gov/medicaid/controllers/admin/report/ApplicationsByReviewerReportController.java
+++ psm-app/cms-web/src/main/java/gov/medicaid/controllers/admin/report/ApplicationsByReviewerReportController.java
@@ -88,7 +88,7 @@ public class ApplicationsByReviewerReportController extends gov.medicaid.control
                 csvPrinter.printRecord(
                     enrollment.getTicketId(),
                     enrollment.getCreatedOn(),
-                    enrollment.getLastUpdatedBy().getUserId(),
+                    enrollment.getLastUpdatedBy().getUsername(),
                     enrollment.getStatusDate(),
                     enrollment.getStatus().getDescription()
                 );

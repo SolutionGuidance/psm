06a3ef1c54f376501435aba3ac35aea8654c43c6
Paul Morris <pwm@paulwmorris.com>
2018-05-23 17:34:36 -0400

    Add files for Time to Review report

---
+++ psm-app/frontend/development-workspace/timeToReviewData.js
@@ -0,0 +1,86 @@
+var reportJson = [
+  {
+    month: "2018-05-01",
+    numberReviewed: "0",
+    meanReviewTime: "0",
+    medianReviewTime: "0"
+  },
+  {
+    month: "2018-04-01",
+    numberReviewed: "0",
+    meanReviewTime: "0",
+    medianReviewTime: "0"
+  },
+  {
+    month: "2018-03-01",
+    numberReviewed: "0",
+    meanReviewTime: "0",
+    medianReviewTime: "0"
+  },
+  {
+    month: "2018-02-01",
+    numberReviewed: "0",
+    meanReviewTime: "0",
+    medianReviewTime: "0"
+  },
+  {
+    month: "2018-01-01",
+    numberReviewed: "0",
+    meanReviewTime: "0",
+    medianReviewTime: "0"
+  },
+  {
+    month: "2017-12-01",
+    numberReviewed: "0",
+    meanReviewTime: "0",
+    medianReviewTime: "0"
+  },
+  {
+    month: "2017-11-01",
+    numberReviewed: "0",
+    meanReviewTime: "0",
+    medianReviewTime: "0"
+  },
+  {
+    month: "2017-10-01",
+    numberReviewed: "0",
+    meanReviewTime: "0",
+    medianReviewTime: "0"
+  },
+  {
+    month: "2017-09-01",
+    numberReviewed: "0",
+    meanReviewTime: "0",
+    medianReviewTime: "0"
+  },
+  {
+    month: "2017-08-01",
+    numberReviewed: "0",
+    meanReviewTime: "0",
+    medianReviewTime: "0"
+  },
+  {
+    month: "2017-07-01",
+    numberReviewed: "0",
+    meanReviewTime: "0",
+    medianReviewTime: "0"
+  },
+  {
+    month: "2017-06-01",
+    numberReviewed: "1",
+    meanReviewTime: "216",
+    medianReviewTime: "216"
+  },
+  {
+    month: "2017-05-01",
+    numberReviewed: "2",
+    meanReviewTime: "254",
+    medianReviewTime: "317"
+  },
+  {
+    month: "2017-04-01",
+    numberReviewed: "1",
+    meanReviewTime: "216",
+    medianReviewTime: "216"
+  }
+];

c3109d266fefcd76f17ddd5f91e00aab60155622
Dan Schultz <slifty@gmail.com>
2018-06-26 23:21:00 -0400

    Create affiliations migration
    Creation of the affiliations table is now handled through liquibase migrations.
    
    Issue #254 Allow database schema changes without losing data

--- psm-app/db/seed.sql
+++ psm-app/db/seed.sql
@@ -1,5 +1,4 @@
 DROP TABLE IF EXISTS
-  affiliations,
   beneficial_owner,
   events,
   external_account_links,
@@ -11,25 +10,6 @@ DROP TABLE IF EXISTS
   provider_services,
   provider_type_settings
 CASCADE;
-
-CREATE TABLE affiliations(
-  affiliation_id BIGINT PRIMARY KEY,
-  is_primary CHARACTER VARYING(1),
-  profile_id BIGINT,
-  object_type TEXT,
-  ticket_id BIGINT,
-  effective_at DATE,
-  target_profile_id BIGINT,
-  target_entity_id BIGINT,
-  qualified_professional_type_code CHARACTER VARYING(2)
-    REFERENCES qualified_professional_types(code),
-  mental_health_professional_type TEXT,
-  acknowledgement_attachment_id TEXT,
-  is_terminated CHARACTER VARYING(1),
-  terminated_at DATE,
-  bgs_study_id TEXT,
-  bgs_clearance_date DATE
-);
 
 CREATE TABLE ownership_info (
   ownership_info_id BIGINT PRIMARY KEY,

ddaa3cecc9e53ac584e4c3aac0174a51cf756a3f
Dan Schultz <slifty@gmail.com>
2018-06-26 23:37:05 -0400

    Create external_account_links migration
    Creation of the external_account_links table is now handled through liquibase migrations.
    
    Issue #254 Allow database schema changes without losing data

--- psm-app/db/seed.sql
+++ psm-app/db/seed.sql
@@ -1,14 +1,6 @@
 DROP TABLE IF EXISTS
-  external_account_links,
   external_profile_links
 CASCADE;
-
-CREATE TABLE external_account_links(
-  external_account_link_id BIGINT PRIMARY KEY,
-  user_id TEXT,
-  system_id TEXT,
-  external_user_id TEXT
-);
 
 CREATE TABLE external_profile_links(
   external_profile_link_id BIGINT PRIMARY KEY,

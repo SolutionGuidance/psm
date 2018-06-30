e78ca84f1f0a878092b4d4077ba906401e2d2b42
Marvin Wolfthal <maw@weichi.com>
2018-06-30 17:28:06 -0400

    Add RulesExecutor to handle KnowledgeSession operations
    Methods invoking KnowledgeSession operations will be moved here
        to isolate Drools namespace as much as possible, starting with
        PresentationServiceBean, ScreeningHandler and
        BusinessProcessServiceBean
    
    Issue #385 Upgrade separate Drools server

---
+++ psm-app/cms-business-process/src/main/java/gov/medicaid/domain/rules/RulesExecutor.java
@@ -0,0 +1,15 @@
+package gov.medicaid.domain.rules;
+
+import java.util.logging.Logger;
+
+/**
+ * This class handles all KnowledgeSession operations.
+ */
+
+public class RulesExecutor {
+    private static final Logger logger =
+        Logger.getLogger(RulesExecutor.class.getName());
+
+    private RulesExecutor() {
+    }
+}

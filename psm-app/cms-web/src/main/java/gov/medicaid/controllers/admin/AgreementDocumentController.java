778c540c3dbf68ce340464bcadb88bc1d9ba5ad2
Jason Owen <jason@jasonaowen.net>
2018-07-03 16:59:34 -0400

    Refactor agreement document controller init
    Use a constructor argument instead of a property, and rely on Spring to
    understand what to inject rather than explicitly specifying
    dependencies.

--- psm-app/cms-web/src/main/java/gov/medicaid/controllers/admin/AgreementDocumentController.java
+++ psm-app/cms-web/src/main/java/gov/medicaid/controllers/admin/AgreementDocumentController.java
@@ -22,7 +22,6 @@ import gov.medicaid.entities.AgreementDocumentSearchCriteria;
 import gov.medicaid.entities.AgreementDocumentType;
 import gov.medicaid.entities.SearchResult;
 import gov.medicaid.services.AgreementDocumentService;
-import gov.medicaid.services.PortalServiceConfigurationException;
 import gov.medicaid.services.PortalServiceException;
 
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -32,7 +31,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 
 import java.security.Principal;
@@ -49,32 +47,10 @@ import java.security.Principal;
  * @version 1.0
  */
 public class AgreementDocumentController extends BaseServiceAdminController {
-    /**
-     * Represents the agreement document service. It is managed with a getter and setter. It may have any value, but is
-     * expected to be set to a non-null/empty value by dependency injection It is fully mutable, but not expected to
-     * change after dependency injection.
-     */
-    private AgreementDocumentService agreementDocumentService;
+    private final AgreementDocumentService agreementDocumentService;
 
-    /**
-     * Empty constructor.
-     */
-    public AgreementDocumentController() {
-    }
-
-    /**
-     * This method checks that all required injection fields are in fact provided.
-     *
-     * @throws PortalServiceConfigurationException If there are required injection fields that are not injected
-     */
-    @Override
-    @PostConstruct
-    protected void init() {
-        super.init();
-
-        if (agreementDocumentService == null) {
-            throw new PortalServiceConfigurationException("agreementDocumentService must be configured.");
-        }
+    public AgreementDocumentController(AgreementDocumentService agreementDocumentService) {
+        this.agreementDocumentService = agreementDocumentService;
     }
 
     /**
@@ -259,23 +235,5 @@ public class AgreementDocumentController extends BaseServiceAdminController {
             agreementDocumentService.delete(agreementId);
         }
         return "success";
-    }
-
-    /**
-     * Getter of agreementDocumentService.
-     *
-     * @return the agreementDocumentService
-     */
-    public AgreementDocumentService getAgreementDocumentService() {
-        return agreementDocumentService;
-    }
-
-    /**
-     * Set the agreementDocumentService.
-     *
-     * @param agreementDocumentService the agreementDocumentService to set
-     */
-    public void setAgreementDocumentService(AgreementDocumentService agreementDocumentService) {
-        this.agreementDocumentService = agreementDocumentService;
     }
 }
